b78620b1dfe2ace6ba7dc218ae87f03353736dc6
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-06 09:26:50 -0300

    Clear Project

---
+++ src/components/home/Main.vue
@@ -0,0 +1,15 @@
+<template>
+	<b-container id="content-main" fluid class="m-0 p-0">
+
+	</b-container>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
