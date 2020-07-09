f868a36ff2d6e2082728c92aac98dcdb42cb41f0
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-09 10:54:26 -0300

    Update router.js

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -3,18 +3,28 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/components/home/Main.vue')
-  }
-]
+	const routes = [
+		{
+			path: '/',
+			name: 'Home',
+			component: () => import('@/components/home/Main.vue')
+		},
+		{
+			path: '/add',
+			name: 'AddProject',
+			component: () => import('@/components/addProject/Main.vue')
+		},
+		{
+			path: '/remove',
+			name: 'RemoveProject',
+			component: () => import('@/components/removeProject/Main.vue')
+		}
+	]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes
 })
 
 export default router
