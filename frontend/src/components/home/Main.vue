6dcefa56b5dd6ef6d0fcec9c37e865c5844cef83
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-08 23:00:48 -0300

    Top Fixe

--- frontend/src/components/home/Main.vue
+++ frontend/src/components/home/Main.vue
@@ -1,27 +1,42 @@
 <template>
 	<b-container id="content-main" fluid class="m-0 p-0">
-		<b-row id="fixed-top" no-gutters style="background-color: #2aadd5">
-			<b-col 
+		<div id="fixed-top" no-gutters style="background-color: #2aadd5">
+			<div 
 			v-for="content in contentTop"
 			:key="content.name"
-			:style="`font-family: ${content.font} font-size: ${content.fontSize}`"
-			cols="10"
-			class="mx-auto"
-			:class="`text-${content.align} ${content.margin}`">
-				<div
-				:class="`${content.inclination}`">
+			:style="`font-size: ${content.fontSize}`"
+			class="mx-2"
+			:class="`text-${content.align} ${content.margin} ${content.area}`">
+				<div :class="content.inclination">
 					{{ content.text }}
 				</div>
-			</b-col>
-			<b-col cols="2" class="content">
+			</div>
+			<div class="buttons ml-auto">
+				<b-button id="adicionar" class="p-0 button-manager m-2">
+					<b-img src="@/assets/add-icon.svg" fluid-grow/>
+				</b-button>
+				<b-button id="remover" class="p-0 button-manager m-2">
+					<b-img src="@/assets/remove-icon.svg" fluid-grow/>
+				</b-button>
+				<b-tooltip target="adicionar">
+					Adicionar
+				</b-tooltip>
+				<b-tooltip target="remover">
+					Remover
+				</b-tooltip>
+			</div>
+			<div class="social mt-auto ml-auto">
 				<b-button 
 				v-for="buttonSocial in buttonsSocial" 
 				:key="buttonSocial.name"
 				class="m-2 p-0 button-social">
-					<b-img width="50px" :src="buttonSocial.icon"/>
+					<b-img :id="buttonSocial.name" width="50px" :src="buttonSocial.icon"/>
 				</b-button>
-			</b-col>
-		</b-row>
+				<b-tooltip :target="buttonSocial.name">
+					{{ buttonSocial.tooltip }}
+				</b-tooltip>
+			</div>
+		</div>
 		<ListProject/>
 	</b-container>
 </template>
@@ -38,38 +53,38 @@ import ListProject from './ListProject.vue'
 				contentTop: [
 					{	name: 'welcome',
 						text: 'BEM-VINDO !',
-						font: 'Bangers, cursive;',
+						area: 'bem-vindo',
 						fontSize: '6em',
+						margin: 'm-2',
 						align: 'center',
-						margin: 'm-0',
-						inclination: 'content'
 					},
 					{	name: 'slogan',
 						text: 'Portifólio de Projetos',
-						font: 'Bangers, cursive;',
+						area: 'title',
 						fontSize: '5em',
+						margin: 'm-2',
 						align: 'left',
-						margin: 'ml-2',
-						inclination: 'content'
 					},
 					{	name: 'create',
 						text: 'Create by: Bruno Alfieri',
-						font: 'Bangers, cursive;',
+						area: 'footer',
 						fontSize: '2em',
-						align: 'right',
 						margin: 'mt-auto',
-						inclination: ''
+						align: 'right',
 					},
 				],
 				buttonsSocial: [
-					{	name: 'icon1',
-						icon: require('@/assets/github-icon.svg')
+					{	name: 'github',
+						icon: require('@/assets/github-icon.svg'),
+						tooltip: 'Github'
 					},
-					{	name: 'icon2',
-						icon: require('@/assets/linkedin-icon.svg')
+					{	name: 'linkedin',
+						icon: require('@/assets/linkedin-icon.svg'),
+						tooltip: 'Linkedin'
 					},
-					{	name: 'icon3',
-						icon: require('@/assets/facebook-icon.svg')
+					{	name: 'facebook',
+						icon: require('@/assets/facebook-icon.svg'),
+						tooltip: 'Facebook'
 					}
 				],
 			
@@ -85,27 +100,63 @@ import ListProject from './ListProject.vue'
 $heightRow: 500px;
 
 #content-main {
-	overflow: hidden;
+	overflow: hidden!important;
 	min-height: 100vh;
 	font-family: 'Bangers', cursive;
 	height: 100%;
+
 	#fixed-top {
 		height: $heightRow;
 		max-width: 100%;
-		color: black;
 		overflow: hidden!important;
-		width: 120%;
+		display: grid;
+		align-items: start;
+		grid-template-rows: 3fr 4fr 1fr;
+		grid-template-columns: 5fr 1fr;
+		grid-template-areas:	"bem-vindo buttons"
+								"title social"
+								" footer footer";
+		color: black;
 		transform: skewY(-2deg);
 		transform-origin: top left;
-		.button-social {
-			border-radius: 50%;
-			background-color: transparent;
-		}
-	}
 
-	.content {
-		transform: skewY(2deg);
-		transform-origin: top left;
+		.bem-vindo{ 
+			grid-area: bem-vindo;
+			transform: skewY(2deg);
+			align-self: flex-end;
+		}
+		.buttons{ 
+			grid-area: buttons;
+			transform: skewY(2deg);
+			transform-origin: top left;
+			align-self: flex-end;
+			.button-manager {
+				width: 50px;
+				height: 50px;
+				border-radius: 50%;
+				background-color: transparent;
+				&:hover {
+					box-shadow: 0px 0px 10px black;
+				}
+			}
+		}
+		.title{ 
+			grid-area: title;
+			transform: skewY(2deg);
+		}
+		.social{ 
+			grid-area: social;
+			transform: skewY(2deg);
+			.button-social {
+				border-radius: 50%;
+				background-color: transparent;
+				&:hover {
+					box-shadow: 0px 0px 10px black;
+				}
+			}
+		}
+		.footer{ grid-area: footer;}
+
 	}
 }
 </style>
