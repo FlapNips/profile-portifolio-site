58b6b92febdacaed00ae164e611f581047bd583e
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-09 21:42:15 -0300

    Componente de icones

---
+++ frontend/src/components/home/TechnologyIcons.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="text-center tecnologia">
+		Presentes:
+		<b-img v-for="icon in icons" :key="icon" width="60" height="60" class="mx-3" :src="icon"/>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			icons: [
+				this.getIcons().html5
+			]
+		}
+	},
+	methods: {
+		getIcons() {
+			return this.$store.state.icons
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.tecnologia { 
+	grid-area: tecnologia;
+	transform: skewY(2deg);
+	transform-origin: top left;
+}
+</style>
