079a7cf4ff9938d3dd344752999f00967b281463
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-09 21:39:07 -0300

    Background App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
 <template>
-	<div>
+	<div id="main-app">
 		<router-view/>
 	</div>
 </template>
+<script>
+export default {
 
+}
+</script>
 <style lang="scss" scoped>
+#main-app {
+	background-color: rgba(38, 156, 192, 0.165);
+}
 </style>
