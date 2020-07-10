d0637ab23ffe86c2d14e72b4518b8719cbd801e7
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-09 21:42:46 -0300

    Update router.js

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -7,17 +7,17 @@ Vue.use(VueRouter)
 		{
 			path: '/',
 			name: 'Home',
-			component: () => import('@/components/home/Main.vue')
+			component: () => import('@/components/home/HomeMain.vue')
 		},
 		{
 			path: '/add',
 			name: 'AddProject',
-			component: () => import('@/components/addProject/Main.vue')
+			component: () => import('@/components/addProject/AddMain.vue')
 		},
 		{
 			path: '/remove',
 			name: 'RemoveProject',
-			component: () => import('@/components/removeProject/Main.vue')
+			component: () => import('@/components/removeProject/RemoveMain.vue')
 		}
 	]
 
