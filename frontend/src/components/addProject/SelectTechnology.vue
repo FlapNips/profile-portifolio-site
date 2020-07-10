e00817fe324327e521bd2e480a30ba01e27bf788
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-10 18:49:55 -0300

    Create SelectTechnology.vue

---
+++ frontend/src/components/addProject/SelectTechnology.vue
@@ -0,0 +1,60 @@
+<template>
+	<b-row>
+		<b-button 
+		v-for="(icon, index) in getIcons()" 
+		:key="icon.id" 
+		@click="setPressed(icon.id)"
+		:class="teste = icon.id"
+		class="button-default m-2 p-0">
+			<b-img width="50" height="50" :src="setIcons(icon[index].name)"/>
+		</b-button>
+	</b-row>
+</template>
+
+<script>
+
+export default {
+
+	data() {
+		return {
+			buttons: []
+		}
+	},
+	methods: {
+		setPressed(id) {
+			this.buttons[id].pressed.$set(true)
+		},
+		setIcons(icon) {
+			console.log('ICON')
+			console.log(icon)
+			return this.$store.state.icons[icon]
+		},
+		getIcons() {
+			const icons = this.$store.state.icons
+			const iconsLength = Object.keys(icons).length
+			for(let i = 0; i < iconsLength; i++) {
+				let informations = { 
+					'id': i, 
+					name: Object.keys(icons)[i], 
+					pressed: false
+				}
+
+				this.$set(this.buttons, `icon${i}`, informations)
+			}
+			console.log(this.buttons)
+			return this.buttons
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+.button-default {
+	background-color: transparent;
+	border: 0!important;
+}
+.buttonteste {
+	background-color: black!important;
+
+}
+</style>
