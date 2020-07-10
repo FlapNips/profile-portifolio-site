69ace4c2bd1d5beb0170afe20a182dca85f3da89
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-10 19:57:53 -0300

    Update SelectTechnology.vue

--- frontend/src/components/addProject/SelectTechnology.vue
+++ frontend/src/components/addProject/SelectTechnology.vue
@@ -1,12 +1,12 @@
 <template>
 	<b-row>
 		<b-button 
-		v-for="(icon, index) in getIcons()" 
+		v-for="(icon) in buttons" 
 		:key="icon.id" 
-		@click="setPressed(icon.id)"
-		:class="teste = icon.id"
+		@click="icon.pressed = !icon.pressed"
+		:class="{ buttonteste: icon.pressed }"
 		class="button-default m-2 p-0">
-			<b-img width="50" height="50" :src="setIcons(icon[index].name)"/>
+			<b-img width="50" height="50" :src="icon.value"/>
 		</b-button>
 	</b-row>
 </template>
@@ -21,30 +21,25 @@ export default {
 		}
 	},
 	methods: {
-		setPressed(id) {
-			this.buttons[id].pressed.$set(true)
-		},
-		setIcons(icon) {
-			console.log('ICON')
-			console.log(icon)
-			return this.$store.state.icons[icon]
-		},
 		getIcons() {
 			const icons = this.$store.state.icons
-			const iconsLength = Object.keys(icons).length
-			for(let i = 0; i < iconsLength; i++) {
-				let informations = { 
-					'id': i, 
-					name: Object.keys(icons)[i], 
-					pressed: false
-				}
-
-				this.$set(this.buttons, `icon${i}`, informations)
+			const lengthIcons = Object.keys(icons).length
+			for(let i = 0; i < lengthIcons; i++) {
+				this.$set(this.buttons, i, {
+					'id': i,
+					'pressed': false,
+					'name': Object.keys(icons)[i],
+					'value': Object.values(icons)[i]
+				})
 			}
+			//this.$set(this.buttons, 0, teste)
 			console.log(this.buttons)
-			return this.buttons
 		}
 	},
+	created() {
+		console.log('create foi')
+		this.getIcons()
+	}
 }
 </script>
 
