7f6cc99b2fe95ce27c6c5f2c7fe35d6368ebc7a6
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-09 21:42:49 -0300

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,12 +4,27 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		icons: {
+			vuejs: require('@/assets/vuejs-icon.svg'),
+			javascript: require('@/assets/javascript-icon.svg'),
+			api: require('@/assets/api-icon.svg'),
+			nodejs: require('@/assets/nodejs-icon.svg'),
+			mysql: require('@/assets/mysql-icon.svg'),
+			psql: require('@/assets/postgresql-icon.svg'),
+			firebase: require('@/assets/firebase-icon.svg'),
+			bootstrap: require('@/assets/bootstrap-icon.svg'),
+			docker: require('@/assets/docker-icon.svg'),
+			html5: require('@/assets/html5-icon.svg'),
+			css3: require('@/assets/css3-icon.svg'),
+			sass: require('@/assets/sass-icon.svg'),
+			json: require('@/assets/json-icon.svg'),
+		}
+	},
+	mutations: {
+	},
+	actions: {
+	},
+	modules: {
+	}
 })
