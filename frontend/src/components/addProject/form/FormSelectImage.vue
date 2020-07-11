4bf7c70f04471c66a49244d5213250d056381414
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-11 17:51:15 -0300

    Create FormSelectImage.vue

---
+++ frontend/src/components/addProject/form/FormSelectImage.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<b-form-file
+		placeholder="Arraste a imagem até aqui ou clique em escolher."
+		drop-placeholder="Solte a imagem aqui !"
+		v-model="file"
+		class="my-3">
+		</b-form-file>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			file: null
+		
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.custom-file-input:lang(en) ~ .custom-file-label::after {
+	content: 'ESCOLHER';
+}
+.button-reset {
+	background-color: rgb(63, 63, 63);
+}
+</style>
