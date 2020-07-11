2b007e0843a95cbb72516cfcf770939681387855
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-11 17:52:06 -0300

    Update Store - state.project + getters + mutations

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -6,26 +6,70 @@ Vue.use(Vuex)
 export default new Vuex.Store({
 	state: {
 		icons: {
-			vuejs: require('@/assets/vuejs-icon.svg'),
-			javascript: require('@/assets/javascript-icon.svg'),
-			api: require('@/assets/api-icon.svg'),
-			nodejs: require('@/assets/nodejs-icon.svg'),
-			mysql: require('@/assets/mysql-icon.svg'),
-			psql: require('@/assets/postgresql-icon.svg'),
-			firebase: require('@/assets/firebase-icon.svg'),
-			bootstrap: require('@/assets/bootstrap-icon.svg'),
-			docker: require('@/assets/docker-icon.svg'),
 			html5: require('@/assets/html5-icon.svg'),
 			css3: require('@/assets/css3-icon.svg'),
 			sass: require('@/assets/sass-icon.svg'),
+			javascript: require('@/assets/javascript-icon.svg'),
+			vuejs: require('@/assets/vuejs-icon.svg'),
+			bootstrap: require('@/assets/bootstrap-icon.svg'),
+			nodejs: require('@/assets/nodejs-icon.svg'),
+			api: require('@/assets/api-icon.svg'),
 			json: require('@/assets/json-icon.svg'),
+			mysql: require('@/assets/mysql-icon.svg'),
+			psql: require('@/assets/postgresql-icon.svg'),
+			firebase: require('@/assets/firebase-icon.svg'),
+			docker: require('@/assets/docker-icon.svg'),
 			empty: require('@/assets/empty-icon.svg'),
+		},
+		project: {
+			title: '',
+			level: "Newbie",
+			introduction: '',
+			learning: '',
+			date: '',
+			file: ''
 		}
 	},
 	mutations: {
+		setTitle(state, payload) {
+			state.project.title = payload
+		},
+		setLevel(state, payload) {
+			state.project.level = payload
+		},
+		setIntroduction(state, payload) {
+			state.project.introduction = payload
+		},
+		setLearning(state, payload) {
+			state.project.learning = payload
+		},
+		setDate(state, payload) {
+			state.project.date = payload
+		},
+		setFile(state, payload) {
+			state.project.file = payload
+		},
 	},
 	actions: {
 	},
-	modules: {
+	getters: {
+		getTitle(state) {
+			return state.project.title
+		},
+		getLevel(state) {
+			return state.project.level
+		},
+		getIntroduction(state) {
+			return state.project.introduction
+		},
+		getLearning(state) {
+			return state.project.learning
+		},
+		getDate(state) {
+			return state.project.date
+		},
+		getFile(state) {
+			return state.project.file
+		},
 	}
 })
