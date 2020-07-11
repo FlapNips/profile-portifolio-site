f0a2649a6bbdadc46eb7ab8bd668760a4e59cf8e
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-11 17:50:39 -0300

    Create FormButtons.vue

---
+++ frontend/src/components/addProject/form/FormButtons.vue
@@ -0,0 +1,30 @@
+<template>
+	<b-row>
+		<b-button 
+		v-for="button in buttons" 
+		:key="button.name" 
+		@click="button.reset" 
+		class="button-reset mx-auto">
+			{{button.title}}
+		</b-button>
+	</b-row>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			buttons: [
+				{	name: 'resetImage',
+					title: 'Resetar Imagem',
+					reset: ''
+				},
+				{	name: 'resetAll',
+				title: 'Resetar Tudo',
+					reset: ''
+				}
+			]
+		}
+	}
+}
+</script>
