ef88ad2808c9e06095e9d0df9f4feff16a0a207a
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-11 23:07:04 -0300

    Update FormDatepicker - Dados vinculados ao STORE

--- frontend/src/components/addProject/form/FormDatepicker.vue
+++ frontend/src/components/addProject/form/FormDatepicker.vue
@@ -1,12 +1,27 @@
 <template>
-	<b-form-datepicker placeholder="Escolha a data finalizada!">
-		
-	</b-form-datepicker>
+	<b-form-datepicker 
+	v-model="datePicker"
+	placeholder="Escolha a data finalizada!" 
+	class="my-3"/>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			value: ''
+		}
+	},
+	computed: {
+		datePicker: {
+			get() {
+				return this.$store.getters.getDate
+			},
+			set(value) {
+				this.$store.commit('setDate', value)
+			}
+		}
+	}
 }
 </script>
 
