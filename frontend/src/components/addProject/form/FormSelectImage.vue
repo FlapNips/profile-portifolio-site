2254b4c987e7b4a2a6ae9237cbf212a6cc616c8d
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-12 09:42:46 -0300

    Update FormSelectImage - Add validação de entensão, alerta de aceito ou recusado

--- frontend/src/components/addProject/form/FormSelectImage.vue
+++ frontend/src/components/addProject/form/FormSelectImage.vue
@@ -3,9 +3,14 @@
 		<b-form-file
 		placeholder="Arraste a imagem até aqui ou clique em escolher."
 		drop-placeholder="Solte a imagem aqui !"
+		accept=".jpeg, .png, .gif, .jpg"
 		v-model="file"
-		class="my-3">
+		:state="validateFile(file)"
+		class="mt-1">
 		</b-form-file>
+		<div id="warning-file-text" v-show="validateFile(file) === false" class="text-right">
+			EXTENSÃO DE ARQUIVO NÃO ACEITO !
+		</div>
 	</div>
 </template>
 
@@ -13,18 +18,40 @@
 export default {
 	data() {
 		return {
-			file: null
+			file: null,
+			acceptExtension: [
+				'jpeg',
+				'gif',
+				'jpg',
+				'png'
+			]
 		
+		}
+	},
+	methods: {
+		validateFile(file) {
+			if(file === (undefined || null)) return null
+			const fileName = file.name
+			const extension = fileName.split('.').pop()
+			try {
+			this.acceptExtension.find(element => {
+				if(element == extension) return true
+			}).first
+			} catch(error) {
+				return false
+			}
+			return true
 		}
 	}
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .custom-file-input:lang(en) ~ .custom-file-label::after {
 	content: 'ESCOLHER';
 }
-.button-reset {
-	background-color: rgb(63, 63, 63);
+#warning-file-text {
+	font-size: 0.8em;
+	color: rgb(122, 0, 0);
 }
 </style>
