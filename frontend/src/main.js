c0779cc10402af4ddc9a4091cce8c50242e5a64b
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-13 21:25:02 -0300

    Update Main - Nova diretriz
    Diretriz que vincula com o textoJSON

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,26 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router/router.js'
 import store from './store/store.js'
+import textPTBR from './textPTBR.json'
 
 Vue.config.productionTip = false
+
+Vue.directive('textJSON', {
+	bind(el, binding) {
+		try {
+			const arrayValue = binding.value.split(".")
+			let valueJSON = textPTBR[arrayValue[0]]
+			for(let i = 1; i < arrayValue.length; i++) {
+				valueJSON = valueJSON[arrayValue[i]]
+			}
+			el.innerHTML = valueJSON
+		} catch(error) {
+			console.log('ERRO NO textJSON:')
+			console.log(error)
+		}
+	}
+
+})
 
 new Vue({
   router,
