9c9139e1e95c72034ce29f9702f8e736853939f5
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-14 23:53:19 -0300

    Update RemoveMain - Layout básico

--- frontend/src/components/removeProject/RemoveMain.vue
+++ frontend/src/components/removeProject/RemoveMain.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-  </div>
+	<div id="layout-remove-main">
+		<b-row class="list-layout" no-gutters>
+			<b-col cols="6">
+				#PROJETO
+			</b-col>
+			<b-col cols>
+				teste
+			</b-col>
+		</b-row>
+	</div>
 </template>
 
 <script>
@@ -9,6 +17,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#layout-remove-main {
+	font-family: 'Bangers', cursive;
+	font-size: 1.3em;
+	height: 100vh;
+	width: 100vw;
+	overflow-x: hidden;
+	display: grid;
+	grid-template-rows: 3fr 80fr 3fr;
+	grid-template-columns: 3fr 80fr 3fr;
+	grid-template-areas:	"margin-top margin-top margin-top"
+							"margin-middle-left content-main margin-middle-right"
+							"margin-bottom margin-bottom margin-bottom";
+	.list-layout {
+		grid-area: content-main;
+		border: 2px solid black;
+		height: max-content;
+		background-color: white;
+	
+	}
+}
 </style>
