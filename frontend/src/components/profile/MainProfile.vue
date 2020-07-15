cc574b5df3a2992600dc60a95f59574eff771411
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-07-14 23:52:51 -0300

    Update MainProfile - Organização da página do perfil

--- frontend/src/components/profile/MainProfile.vue
+++ frontend/src/components/profile/MainProfile.vue
@@ -1,52 +1,54 @@
 <template>
-	<div id="layout-profile-main" class="p-4">
-		<div id="layout-personal-profile">
-			<b-img id="image-profile" src="@/assets/logo.png"/>
-			<div id="contact-profile" v-textJSON="`profile.teste.teste2.teste3`">
-			</div>
-			<div id="aboutme-profile">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aspernatur officia voluptates dolor. Dolore quidem tempore, soluta qui magni doloremque sit sint exercitationem, ab consequatur rem distinctio tenetur perspiciatis fugit.
-			</div>
-			<div id="full-name-profile">
-				<div>BRUNO ALFIERI</div>
-				<div>Developer Web</div>
-			</div>
+	<div id="layout-profile-main" class="m-0 pt-3">
+		<Contact/>
+		<AboutMe/>
+		<div class="tag-layout mx-auto">
+			<div v-textJSON="'menu.skills'" class="text-inclination"/>
 		</div>
+		<Skills/>
 	</div>
 </template>
 
 <script>
+import Contact from './Contact'
+import AboutMe from './AboutMe'
+import Skills from './Skills'
 export default {
-
+	components: {
+		Contact,
+		AboutMe,
+		Skills
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 #layout-profile-main {
-	background-color: rgb(197, 197, 197);
-	width: 100vw;
-	min-height: 100vh;
-	#layout-personal-profile {
-		width: 100%;
-		height: 300px;
-		display: grid;
-		grid-template-areas:	"image-profile contact contact about-me"
-								"full-name full-name social social";
-		grid-template-rows: 5fr 2fr;
-		grid-template-columns: 2fr 3fr 3fr 6fr;
-	}
-	#image-profile {
-		grid-area: image-profile;
-	}
-	#contact-profile {
-		grid-area: contact;
-	}
-	#aboutme-profile {
-		grid-area: about-me;
-	}
-	#full-name-profile {
-		grid-area: full-name;
-		font-size: 2em;
-	}
+	background-color: rgb(255, 255, 255);
+	width: 100%;
+	display: grid;
+	grid-template-columns: 2.1fr 2fr 4fr 2.1fr;
+	grid-template-rows: max-content max-content 1fr;
+	grid-template-areas:	"margin-left contact aboutme margin-right"
+							"margin-left tagskills space margin-right"
+							"margin-left skills skills margin-right";
+	
+		.tag-layout {
+			grid-area: tagskills;
+			width: 200px;
+			background-color: rgb(86, 11, 116);
+			height: max-content;
+			margin: 20px 0;
+			color: white;
+			text-align: center;
+			transform: skewX(-20deg);
+				.text-inclination {
+					transform: skewX(20deg);
+					font-size: 1.5em;
+					line-height: 1.5em;
+				}
+		}
 }
+
+
 </style>
