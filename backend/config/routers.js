e1ac00f077f9f533598af6876777454cc3035cb8
Bruno Alfieri <alfieri@outlook.com.br>
2020-07-28 20:24:22 -0300

    BACKEND - Update Routers

--- backend/config/routers.js
+++ backend/config/routers.js
@@ -1,9 +1,39 @@
 module.exports = app => {
+
 	app.route('/teste')
-		.get(app.api.aboutMeTechnologies.getTechnologies)
+		.get(app.api.aboutMeTechnologies.getAllTechnologies)
 		.post(app.api.aboutMeTechnologies.addTechnologies)
-	app.route('/alltechnologies')
-		.get(app.api.allTechnologies.getAllTechnologies)
+//-----------------EXPERIENCES-------------------------
+	app.route('/experience')
+		.post(app.api.experiences.addExperience)
+		.put(app.api.experiences.updateExperience)
+
+	app.route('/experience/:id')
+		.get(app.api.experiences.getExperience)
+		.delete(app.api.experiences.deleteExperience)
+	
 	app.route('/experiences')
-		.get(app.api.experiences.getExperiences)
+		.get(app.api.experiences.getAllExperiences)
+//-----------------TECHNOLOGY--------------------------
+		//UNIQUES
+	app.route('/technology')
+		.post(app.api.technologies.addTechnology)
+		.put(app.api.technologies.updateTechnology)
+
+	app.route('/technology/:id')
+		.get(app.api.technologies.getTechnology)
+		.delete(app.api.technologies.deleteTechnology)
+
+		//ALL
+	app.route('/alltechnologies')
+		.get(app.api.technologies.getAllTechnologies)
+//-----------------USERS-------------------------------
+
+	//API USERS
+	app.route('/user')
+		.post(app.api.users.addUser)
+		.get(app.api.users.getUser)
+		.put(app.api.users.updateUser)
+	app.route('/user/:id')
+		.delete(app.api.users.deleteUser)
 }
