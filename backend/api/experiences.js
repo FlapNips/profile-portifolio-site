ba28b49028d2635059c7369f9ae872f042fed835
Bruno Alfieri <alfieri@outlook.com.br>
2020-07-28 20:23:35 -0300

    BACKEND - Update experience

--- backend/api/experiences.js
+++ backend/api/experiences.js
@@ -1,31 +1,44 @@
 module.exports = app => {
 	const { existsOrError} = app.api.validator
-	const getExperiences = (req, res) => {
+
+	const addExperience = (req, res) => {
+		const experience = { ... req.body }
+		try {
+			existsOrError(experience.title, 'Defina um titulo.')
+			existsOrError(experience.dateConcluded, 'Defina a data concluído.')
+			existsOrError(experience.durationExperience, 'Defina a duração da experiência.')
+			existsOrError(experience.contentActivity, 'Descreva as atividades.')
+			existsOrError(experience.contentTechnologies, 'Escolhar as tecnologias usadas.')
+		} catch(error) {
+			res.status(400).send(error)
+		}
+	
+	}
+
+	const getExperience = (req, res) => {
+	
+	}
+
+	const updateExperience = (req, res) => {
+	
+	
+	}
+
+	const deleteExperience = (req, res) => {
+	
+	}
+	const getAllExperiences = (req, res) => {
 		app.db('experiences_aboutme')
 			.column('title', 'date_concluded', 'duration_experience', 'content_activity', 'content_technologies')
 			.then(x => res.status(200).send(x))
 			.catch(error => res.status(400).send(error))
 	
 	}
-	const addExperience = (req, res) => {
-		const add = { ... req.body }
-		try {
-			existsOrError(add.title, 'Defina um titulo.')
-			existsOrError(add.dateConcluded, 'Defina a data concluído.')
-			existsOrError(add.durationExperience, 'Defina a duração da experiência.')
-			existsOrError(add.contentActivity, 'Descreva as atividades.')
-			existsOrError(add.contentTechnologies, 'Escolhar as tecnologias usadas.')
-		} catch(error) {
-			res.status(400).send(error)
+	return {
+		addExperience,
+		getExperience,
+		updateExperience,
+		deleteExperience,
+		getAllExperiences
 		}
-	
-	}
-	const modifyExperience = (req, res) => {
-	
-	
-	}
-	const removeExperience = (req, res) => {
-	
-	}
-	return { getExperiences }
 }
