723e8b4900e11bd74806b8ad29c10ddcd4e591a6
Bruno Alfieri <alfieri@outlook.com.br>
2020-07-30 19:49:17 -0300

    backend - Create api aboutme

--- backend/api/aboutMe.js
+++ backend/api/aboutMe.js
@@ -1,10 +1,47 @@
 module.exports = app => {
+
+  const {existsOrError} = app.api.validator
+
   const getAboutme = async (req, res) => {
     return app.db('aboutme')
                     .select()
+                    .first()
                     .then( (data => res.status(201).json(data)))
                     .catch( error => res.status(400))
   }
 
-  return { getAboutme }
+  const updateAboutme = async (req, res) => {
+    const data = { ...req.body }
+    const links = await app.db('aboutme')
+                    .select()
+                    .first()
+    console.log(links)
+    try {
+      if(!data.address  || data.address   == '') data.address  = links.address
+      if(!data.phone    || data.phone     == '') data.phone    = links.phone
+      if(!data.email    || data.email     == '') data.email    = links.email
+      if(!data.github   || data.github    == '') data.github   = links.link_github
+      if(!data.linkedin || data.linkedin  == '') data.linkedin = links.link_linkedin
+      if(!data.facebook || data.facebook  == '') data.facebook = links.link_facebook
+    } catch(error) {
+      res.sendStatus(400)
+    }
+
+    app.db('aboutme')
+      .update({
+        address: data.address,
+        phone: data.phone,
+        email: data.email,
+        link_github: data.github,
+        link_linkedin: data.linkedin,
+        link_facebook: data.facebook
+      })
+      .then( () => app.db('aboutme')
+                    .select()
+                    .first()
+                    .then( (data => res.status(201).json(data)))
+                    .catch( error => res.status(400)) )
+      .catch( error => res.send(error) )
+  }
+  return { getAboutme, updateAboutme }
 }
